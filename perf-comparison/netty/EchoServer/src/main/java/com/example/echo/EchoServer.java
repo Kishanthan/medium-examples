35335439042a2db1b51d43ec7a629657b8a632f0
Kishanthan <kshanth2101@gmail.com>
2018-12-24 10:00:35 +0530

    Add netty echo server

---
+++ perf-comparison/netty/EchoServer/src/main/java/com/example/echo/EchoServer.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.example.echo;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.DefaultHttpResponse;
+import io.netty.handler.codec.http.DefaultLastHttpContent;
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.HttpVersion;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+
+public class EchoServer {
+
+    private final int port;
+
+    public EchoServer(int port) {
+
+        this.port = port;
+    }
+
+    private void start() throws Exception {
+
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(group)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) {
+
+                            ch.pipeline().addLast(new EchoServerHandler());
+                            ch.pipeline().addLast(new HttpServerCodec());
+                        }
+                    });
+            ChannelFuture f = b.bind("localhost", port).sync();
+            System.out.println("EchoServer started on : " + f.channel().localAddress());
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        new EchoServer(8686).start();
+    }
+}
+
+class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+
+        HttpResponse httpResponse = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+        httpResponse.headers().set("Content-Type", "application/json");
+        httpResponse.headers().set("Content-Length", "{\"hello\": \"world\"}".getBytes().length);
+        ctx.channel().writeAndFlush(httpResponse);
+        HttpContent httpContent = new DefaultLastHttpContent(Unpooled.
+                wrappedBuffer(ByteBuffer.wrap("{\"hello\": \"world\"}".getBytes())));
+        ctx.channel().writeAndFlush(httpContent);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
